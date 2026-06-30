95ad9269a15eb9c2ba1183a7d12ca9bc4725cd12
Mayun Suryatama <mayunngrh@gmail.com>
2026-06-30 23:10:16 +0800

    Initial Commit

---
+++ SimpleBotCil/SimpleBotCilApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleBotCilApp.swift
+//  SimpleBotCil
+//
+//  Created by Mayun Suryatama on 30/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SimpleBotCilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
